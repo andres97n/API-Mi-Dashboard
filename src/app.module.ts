8c5b48f263e6a3a40422fd1c686192f0cf98ad93
Andrés Novillo <novilloa21@gmail.com>
2025-07-05 21:00:51 -0500

    app.module - Refactor serie module added /serie - Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SerieModule } from './serie/serie.module';
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.DB_URI ?? 'mongodb://localhost:27017/mi-dashboard')
+    MongooseModule.forRoot(process.env.DB_URI ?? 'mongodb://localhost:27017/mi-dashboard'),
+    SerieModule
   ],
   controllers: [],
   providers: [],
