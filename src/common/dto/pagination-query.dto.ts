4a4a628f6cda046811b09cb14ad002ab20ddd89e
Andrés Novillo <novilloa21@gmail.com>
2025-07-06 19:07:42 -0500

    MERGE main

---
+++ src/common/dto/pagination-query.dto.ts
@@ -0,0 +1,17 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Min } from 'class-validator';
+
+
+export class PaginationQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number;
+}
---
+++ src/common/helpers/pagination.helper.ts
@@ -0,0 +1,29 @@
+import { FilterQuery, Model, ProjectionType, SortOrder } from 'mongoose';
+
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+// import { PaginatedResult } from '../../../dist/common/interfaces/paginate.interface';
+
+
+export async function paginate<T>(
+  model: Model<T>,
+  paginationQuery: PaginationQueryDto,
+  filter: FilterQuery<T> = {},
+  sort: Record<string, SortOrder> = { createdAt: -1 },
+  projection?: ProjectionType<T> | null,
+) {
+  const { limit = 10, page = 1 } = paginationQuery;
+  const skip = (page - 1) * limit;
+
+  const [data, total] = await Promise.all([
+    model.find(filter, projection).sort(sort).skip(skip).limit(limit).exec(),
+    model.countDocuments(filter).exec(),
+  ]);
+
+  return {
+    data,
+    total,
+    page,
+    limit,
+    totalPages: Math.ceil(total / limit),
+  };
+}
